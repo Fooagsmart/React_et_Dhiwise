6009a0fbe93d117ad9b70b577f56df3be367cb82
Fooagsmart <adunmofestus@gmail.com>
2023-12-03 10:44:57 +0000

    Code Synced by DhiWise

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ export { Button } from "./Button";
 export { Img } from "./Img";
 export { Input } from "./Input";
 export { List } from "./List";
-export { ProtectedRoute } from "./ProtectedRoute";
 export { Text } from "./Text";
 export { TextArea } from "./TextArea";
